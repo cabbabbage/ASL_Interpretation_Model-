1336eeb8481f56ef147cdfedceb9aca2818d4b41
Jakob Pharazyn <111919584+jakphaz@users.noreply.github.com>
2024-10-27 18:10:03 -0600

    Restructured Consent to create/close UI and edit csv

--- consent.py
+++ consent.py
@@ -1,68 +1,79 @@
 from tkinter import *
 from tkinter import ttk
+import numpy as np
+import pandas as pd
 
 class Consent():
     #dunderscore is used in python to denote private variables
-    __Username : str
-    __age : int
     __UserID : str
-    IDNum = -1 #Static variable for ID; will need to change later if not done consecutively
+    __form : any
+    __filename : str
 
-    def __init__(self, Username : str, age : int):
-        Consent.IDNum += 1
-        self.__UserID = "P" + str(Consent.IDNum)
-        self.__Username = Username
-        self.__age = age
+    def __init__(self, filename = "form.csv"):
+        self.__root = Tk()
+        self.__filename = filename
+
+        self.__openForm()
+
+        self.__root.title("Consent Form")
+
+        self.__mainframe = ttk.Frame(self.__root, padding="3 3 24 24")
+        self.__mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.__root.columnconfigure(0, weight=1)
+        self.__root.rowconfigure(0, weight=1)
+        self.__root.geometry("400x300")
+
+        self.__name = StringVar()
+        self.__name_entry = ttk.Entry(self.__mainframe, width=10, textvariable=self.__name)
+        self.__name_entry.grid(column=2, row=1, sticky=(W, E))
+
+        self.__age = IntVar()
+        self.__age_entry = ttk.Entry(self.__mainframe, width=2, textvariable=self.__age)
+        self.__age_entry.grid(column=2, row=2, sticky=(W, E))
+
+        ttk.Label(self.__mainframe, text="Name:").grid(column=1, row=1, sticky=W)
+        ttk.Label(self.__mainframe, text="Age:").grid(column=1, row=2, sticky=W)
+
+        ttk.Button(self.__mainframe, text="Submit", command=self.__makeForm).grid(column=3, row=3, sticky=W)
+
+        for child in self.__mainframe.winfo_children(): 
+            child.grid_configure(padx=30, pady=30)
+
+        self.__name_entry.focus()
+        self.__age_entry.focus()
+        self.__root.bind("<Return>", self.__makeForm)
+
+        self.__root.mainloop()
+
+    def __openForm(self):
+        try:
+            self.__form = pd.read_csv(self.__filename, delimiter=',', index_col=0)
+        except:
+            print("Error! : Could not load from form: " + self.__filename)
+
+    def __makeForm(self, *args):
+        self.__UserID = "P" + str(len(self.__form))
+        info = pd.DataFrame([[self.__UserID, self.__age.get()]])
+        self.__savetoForm(info)
+        self.__root.destroy()
+
+    def __savetoForm(self, info):
+        try:
+            print(info)
+            info.to_csv(self.__filename, index=False, mode='a', header=False)
+        except:
+            print("Error! : Could not save to form: " + self.__filename)
 
     def getName(self):
-        return self.__Username
+        return self.__name.get()
     
     def getAge(self):
-        return self.__age
+        return self.__age.get()
     
     def getUserID(self):
         return self.__UserID
-    
-def makeForm(*args):
-    try:
-        form = Consent(name.get(), age.get())
-        print("Name: " + form.getName())
-        print("Age: " + str(form.getAge()))
-        print("UID: " + form.getUserID())
-        return form
-    except ValueError:
-        pass
+   
 
 
 if __name__ == "__main__":
-    root = Tk()
-    root.title("Consent Form")
-
-    mainframe = ttk.Frame(root, padding="3 3 24 24")
-    mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-    root.columnconfigure(0, weight=1)
-    root.rowconfigure(0, weight=1)
-    root.geometry("400x300")
-
-    name = StringVar()
-    name_entry = ttk.Entry(mainframe, width=10, textvariable=name)
-    name_entry.grid(column=2, row=1, sticky=(W, E))
-
-    age = IntVar()
-    age_entry = ttk.Entry(mainframe, width=2, textvariable=age)
-    age_entry.grid(column=2, row=2, sticky=(W, E))
-
-    ttk.Label(mainframe, text="Name:").grid(column=1, row=1, sticky=W)
-    ttk.Label(mainframe, text="Age:").grid(column=1, row=2, sticky=W)
-
-    ttk.Button(mainframe, text="Submit", command=makeForm).grid(column=3, row=3, sticky=W)
-
-    for child in mainframe.winfo_children(): 
-        child.grid_configure(padx=30, pady=30)
-
-    name_entry.focus()
-    age_entry.focus()
-    root.bind("<Return>", makeForm)
-
-    root.mainloop()
-
+    consent = Consent()
